10a226a2e8c25a44b5f25119068869dc3c7f00a1
mledpal <mledpal@gmail.com>
2024-11-16 20:59:30 +0100

    Modificación en tiempos y curvas. La imagen se mueve más dinámicamente

--- moscas_tv/js/moscas.js
+++ moscas_tv/js/moscas.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	const ancho_container = (window.innerWidth * 0.8).toFixed(0);
 	container.style.width = `${ancho_container}px`;
 
-	const numero_columnas = 30;
+	const numero_columnas = 50;
 	const empezar_a_desaparecer = 5000;
 	const tiempo_desaparecer = 2000;
 
@@ -27,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function () {
 	const endColor = [9, 68, 98]; // RGB de var(--color-azul-oscuro)
 
 	// FPV EN JAÉN
-	// const startColor = [77, 211, 39]; 
-	// const endColor = [1, 1, 1]; 
+	// const startColor = [77, 211, 39];
+	// const endColor = [1, 1, 1];
 
 	for (let i = 0; i < numero_columnas * filas; i++) {
 		const div = document.createElement("div");
@@ -56,11 +56,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	setTimeout(function () {
 		logo.classList.add("aparecerTexto");
-	}, tiempo_desaparecer - 1000);
+	}, tiempo_desaparecer - 2000);
 	setTimeout(() => {
 		logo.classList.remove("aparecerTexto");
 		logo.classList.add("desaparecerTexto");
-	}, empezar_a_desaparecer - 500);
+	}, empezar_a_desaparecer + 500);
 
 	setTimeout(function () {
 		const moscas = document.querySelectorAll(".mosca");
