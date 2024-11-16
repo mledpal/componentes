63f3240e4d4bf6bac865b6109e4c5089eb4226f7
mledpal <mledpal@gmail.com>
2024-11-16 19:36:04 +0100

    Se genera un degradado al final

--- moscas_tv/js/moscas.js
+++ moscas_tv/js/moscas.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	container.style.width = `${ancho_container}px`;
 
 	const numero_columnas = 30;
-	const empezar_a_desaparecer = 10000;
+	const empezar_a_desaparecer = 5000;
 	const tiempo_desaparecer = 2000;
 
 	const ancho = container.offsetWidth;
@@ -22,26 +22,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const filas = Math.floor(alto / ancho_div);
 
-	// Calcular el máximo delay basado en la distancia más grande
-	const max_distance = Math.sqrt((filas - 1) ** 2 + (numero_columnas - 1) ** 2);
+	// Definir colores del degradado
+	const startColor = [102, 195, 207]; // RGB de var(--color-azul-claro)
+	const endColor = [9, 68, 98]; // RGB de var(--color-azul-oscuro)
 
 	for (let i = 0; i < numero_columnas * filas; i++) {
 		const div = document.createElement("div");
 		div.style.width = ancho_div + "px";
 		div.setAttribute("data-id", i);
 
-		// Calcular fila y columna de cada div
 		const fila = Math.floor(i / numero_columnas);
 		const columna = i % numero_columnas;
 
-		// Calcular distancia desde la esquina superior izquierda
-		const distance = Math.sqrt(fila ** 2 + columna ** 2);
+		const normalizedCol = columna / (numero_columnas - 1);
+		const normalizedRow = fila / (filas - 1);
 
-		// Calcular delay proporcional a la distancia
-		const delay = (distance / max_distance) * (tiempo_desaparecer / 1000);
-		div.style.animationDelay = `${delay}s`;
+		const t = (normalizedCol + normalizedRow) / 2;
+		const interpolatedColor = startColor.map((start, index) =>
+			Math.round(start + t * (endColor[index] - start))
+		);
+		div.style.backgroundColor = `rgb(${interpolatedColor.join(",")})`;
 
+		// Agregar animaciones
+		div.style.animationDelay = `${(fila + columna) * 0.05}s`;
 		div.classList.add("mosca");
+
 		container.appendChild(div);
 	}
 
